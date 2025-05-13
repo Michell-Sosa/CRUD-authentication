ea541fd88ee80fd42fdfc1cd950acafd9d5b3f36
Michell <michellsosa89qgmail.com>
2025-05-13 11:29:06 -0600

    Set up del backend y adicion de modulos necesarios

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import express from "express"  //importando express
+
+const app = express ()  //inicializando express ejecutandolo y guardando el objeto devuelto en una constante app
+
+export default app;  //una vez inicializado el app, se exportara 
