a19e95cc2c6cade25b11186e097b6f446d805a68
Michell <michellsosa89qgmail.com>
2025-05-16 12:43:11 -0600

    Se realiza token y se agregan validaciones de este mismo

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -36,4 +36,42 @@ export const register = async  (req, res ) => {
 
 
 }
-export const login = (req, res ) => res.send('login');
+export const login = async  (req, res ) => {
+    const {email, password} = req.body // se espera que para el request body se envie email, password y username 
+
+    try {
+
+        const userFound = await User.findOne({email}) //en este caso se va a buscar por correo y userFound va a guardarlo si es que ya existe
+
+        if (!userFound) return res.status(400).json({ message: 'User not found'}); //si no encuentra el mail entonces mostrara este mensaje
+
+
+        const isMatch = await bcrypt.compare(password, userFound.password); //aqui se esta pidiendo a bcrypt que compare las password de la DB y del userFound
+
+        if (!isMatch) return res.status(400).json ({ message: 'Invalid credentials'}); //si la respuesta es false, se muestra este mensaje
+
+            
+    const token = await createAccessToken({id: userFound._id});  //se va a guardar el id de userFound y crear el token con el
+        res.cookie('token', token)   //en este caso el token se guardara en una cookie
+        res.json({   //aqui se especifica cuales valores tendran que ser devueltos para ser usados por el frontend 
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    });
+    } catch (error) {
+        res.status(500).json ({ message: error.message});
+    }
+
+
+
+}
+
+
+export const logout = (req, res) => {
+    res.cookie('token', '', {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express' //se importa la funcion router para crear un enrutador
-import {login, register} from '../controllers/auth.controller.js'
+import {login, logout, register} from '../controllers/auth.controller.js'
 
 
 
@@ -13,6 +13,8 @@ const router = Router() //cuando router se ejecuta brinda un objeto nuevo, el cu
 //se crea la ruta llamada register y login, estas son las tipicas
 router.post ('/register', register); // lo que se ejecutara cuando estas sean llamadas
 router.post ('/login', login); // se desarrolla en controllers
+router.post ('/logout', logout); //
+
 
 
 //el router sera exportado para poder agregarlo en la aplicacion de express
